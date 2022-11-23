bbf9d9d0ef608b9a0401a37bbacf5104ec03f194
tanya-lyubimaya <tatyana.lyubimaya.work@gmail.com>
2022-11-23 18:09:06 +0300

    🎉 Initial commit

---
+++ main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/google/go-github/v48/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	FILENAME string = "update_me.yaml"
+)
+
+type fileStruct struct {
+	UpdateTimes int    `yaml:"update_times"`
+	LastUpdate  string `yaml:"last_update"`
+}
+
+var (
+	sourceOwner   = flag.String("source-owner", "tanya-lyubimaya", "Name of the owner (user or org) of the repo to create the commit in.")
+	sourceRepo    = flag.String("source-repo", "regular-commit", "Name of repo to create the commit in.")
+	commitMessage = flag.String("commit-message", "💥 Updated file "+FILENAME, "Content of the commit message.")
+	commitBranch  = flag.String("commit-branch", "main", "Name of branch to create the commit in. If it does not already exists, it will be created using the `base-branch` parameter")
+	baseBranch    = flag.String("base-branch", "main", "Name of branch to create the `commit-branch` from.")
+	sourceFiles   = flag.String("files", FILENAME, `Comma-separated list of files to commit and their location.
+The local file is separated by its target location by a semi-colon.
+If the file should be in the same location with the same name, you can just put the file name and omit the repetition.
+Example: README.md,main.go:github/examples/commitpr/main.go`)
+	authorName  = flag.String("author-name", "Tatyana Lyubimaya", "Name of the author of the commit.")
+	authorEmail = flag.String("author-email", "tatyana.lyubimaya.work@gmail.com", "Email of the author of the commit.")
+)
+
+var ctx = context.Background()
+var client *github.Client
+
+func main() {
+	flag.Parse()
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	var f fileStruct
+
+	readYamlFile(FILENAME, &f)
+	updateYamlFile(FILENAME, &f)
+
+	if token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
+	if *sourceOwner == "" || *sourceRepo == "" || *commitBranch == "" || *sourceFiles == "" || *authorName == "" || *authorEmail == "" {
+		log.Fatal("You need to specify a non-empty value for the flags `-source-owner`, `-source-repo`, `-commit-branch`, `-files`, `-author-name` and `-author-email`")
+	}
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client = github.NewClient(tc)
+
+	ref, err := getRef()
+	if err != nil {
+		log.Fatalf("Unable to get/create the commit reference: %s\n", err)
+	}
+	if ref == nil {
+		log.Fatalf("No error where returned but the reference is nil")
+	}
+
+	tree, err := getTree(ref)
+	if err != nil {
+		log.Fatalf("Unable to create the tree based on the provided files: %s\n", err)
+	}
+
+	if err := pushCommit(ref, tree); err != nil {
+		log.Fatalf("Unable to create the commit: %s\n", err)
+	}
+}
+
+func readYamlFile(filename string, info *fileStruct) error {
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, info)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	if err != nil {
+		panic("Unable to read data from the file")
+	}
+	return nil
+}
+
+func updateYamlFile(fileName string, previousInfo *fileStruct) error {
+	yamlData, err := yaml.Marshal(&fileStruct{
+		previousInfo.UpdateTimes + 1,
+		time.Now().String(),
+	})
+
+	if err != nil {
+		fmt.Printf("Error while Marshaling. %v", err)
+	}
+
+	err = ioutil.WriteFile(fileName, yamlData, 0644)
+	if err != nil {
+		panic("Unable to write data into the file")
+	}
+	return nil
+}
+
+func getRef() (ref *github.Reference, err error) {
+	if ref, _, err = client.Git.GetRef(ctx, *sourceOwner, *sourceRepo, "refs/heads/"+*commitBranch); err == nil {
+		return ref, nil
+	}
+
+	// We consider that an error means the branch has not been found and needs to
+	// be created.
+	if *commitBranch == *baseBranch {
+		return nil, errors.New("the commit branch does not exist but `-base-branch` is the same as `-commit-branch`")
+	}
+
+	if *baseBranch == "" {
+		return nil, errors.New("the `-base-branch` should not be set to an empty string when the branch specified by `-commit-branch` does not exists")
+	}
+
+	var baseRef *github.Reference
+	if baseRef, _, err = client.Git.GetRef(ctx, *sourceOwner, *sourceRepo, "refs/heads/"+*baseBranch); err != nil {
+		return nil, err
+	}
+	newRef := &github.Reference{Ref: github.String("refs/heads/" + *commitBranch), Object: &github.GitObject{SHA: baseRef.Object.SHA}}
+	ref, _, err = client.Git.CreateRef(ctx, *sourceOwner, *sourceRepo, newRef)
+	return ref, err
+}
+
+// getTree generates the tree to commit based on the given files and the commit
+// of the ref you got in getRef.
+func getTree(ref *github.Reference) (tree *github.Tree, err error) {
+	// Create a tree with what to commit.
+	entries := []*github.TreeEntry{}
+
+	// Load each file into the tree.
+	for _, fileArg := range strings.Split(*sourceFiles, ",") {
+		file, content, err := getFileContent(fileArg)
+		if err != nil {
+			return nil, err
+		}
+		entries = append(entries, &github.TreeEntry{Path: github.String(file), Type: github.String("blob"), Content: github.String(string(content)), Mode: github.String("100644")})
+	}
+
+	tree, _, err = client.Git.CreateTree(ctx, *sourceOwner, *sourceRepo, *ref.Object.SHA, entries)
+	return tree, err
+}
+
+// getFileContent loads the local content of a file and return the target name
+// of the file in the target repository and its contents.
+func getFileContent(fileArg string) (targetName string, b []byte, err error) {
+	var localFile string
+	files := strings.Split(fileArg, ":")
+	switch {
+	case len(files) < 1:
+		return "", nil, errors.New("empty `-files` parameter")
+	case len(files) == 1:
+		localFile = files[0]
+		targetName = files[0]
+	default:
+		localFile = files[0]
+		targetName = files[1]
+	}
+
+	b, err = os.ReadFile(localFile)
+	return targetName, b, err
+}
+
+// pushCommit creates the commit in the given reference using the given tree.
+func pushCommit(ref *github.Reference, tree *github.Tree) (err error) {
+	// Get the parent commit to attach the commit to.
+	parent, _, err := client.Repositories.GetCommit(ctx, *sourceOwner, *sourceRepo, *ref.Object.SHA, nil)
+	if err != nil {
+		return err
+	}
+	// This is not always populated, but is needed.
+	parent.Commit.SHA = parent.SHA
+
+	// Create the commit using the tree.
+	date := time.Now()
+	author := &github.CommitAuthor{Date: &date, Name: authorName, Email: authorEmail}
+	commit := &github.Commit{Author: author, Message: commitMessage, Tree: tree, Parents: []*github.Commit{parent.Commit}}
+	newCommit, _, err := client.Git.CreateCommit(ctx, *sourceOwner, *sourceRepo, commit)
+	if err != nil {
+		return err
+	}
+
+	// Attach the commit to the master branch.
+	ref.Object.SHA = newCommit.SHA
+	_, _, err = client.Git.UpdateRef(ctx, *sourceOwner, *sourceRepo, ref, false)
+	return err
+}
